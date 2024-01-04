a39de4c700035fee55467c4271b990d5fbf73e3d
Fleur41 <Muriithikings456@gmail.com>
2024-01-04 11:22:03 +0300

    Final Queu

--- Que.py
+++ Que.py
@@ -16,9 +16,29 @@ class Queue:
     def enqueue(self, item):
         self.items.append(item)
 
+    def size(self):
+       return len(self.items)
+
     def dequeue(self):
         return self.items.pop(0)
     
+    def peek(self):
+        if not self.is_empty():
+            return self.items[0]
+        else:
+            return None
     
+if __name__ == '__main__':
+    q = Queue()
+    print(q.is_empty())
+    q.enqueue('First')
+    q.enqueue('Second')
+    print(q)
+    print(q.dequeue())
+    print(q)
+    print(q.size())
+    print(q.peek())
+    
+
 
     
