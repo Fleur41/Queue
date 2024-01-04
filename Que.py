11d57e3f738c045cfb009770a96d91eafd29c049
Fleur41 <Muriithikings456@gmail.com>
2024-01-04 11:07:48 +0300

    is_empty

--- Que.py
+++ Que.py
@@ -2,3 +2,13 @@
 Queue Data Structure
 -------------------------------------------------------------
 '''
+
+class Queue:
+    def __init__(self) -> None:
+        self.items = []
+
+    def __repr__(self) -> str:
+        return f'Queue object: data={self.items}'
+    
+    def is_empty(self):
+        return not self.items
